855cae784af9030faa59545efd7a71e795a4730a
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-06 15:10:26 +0530

    sidebar created

--- client/src/componets/Header/Header.jsx
+++ client/src/componets/Header/Header.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 import './Header.css'
+import { BiMenuAltRight } from "react-icons/bi";
+import { useState } from 'react';
+import OutSideClickHandler from "react-outside-click-handler";
+
 
 const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false)
+
+  const getMenuStyles = (menuOpened)=> { 
+    if (document.documentElement.clientWidth <= 800)
+       {
+    return{right: !menuOpened && "-100%" };
+  }
+};
   return (
     <section className="h-wrapper">
       <div className="flexCenter innerWidth paddings h-container">
         <img src="./logo.png" alt="logo" width={100} />
-
-        <div className="flexCenter h-menu">
+      <OutSideClickHandler
+        onOutsideClick={()=> {
+          setMenuOpened(false)
+        }}>
+        <div className="flexCenter h-menu" style={getMenuStyles(menuOpened)}>
           <a href="">Residencies</a>
           <a href="">Our Value</a>
           <a href="">Contact Us</a>
@@ -15,7 +30,10 @@ const Header = () => {
           <button className="button">
           <a href="">Contact</a>
           </button>
-
+        </div>
+        </OutSideClickHandler>
+        <div className="menu-icon" onClick={()=>setMenuOpened((prev)=>!prev)}>
+          <BiMenuAltRight size={30}/>
         </div>
       </div>
     </section>
