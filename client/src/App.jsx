ad63871c64889dc3064436ceb6e2c6593b424458
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-06 12:25:09 +0530

    contact page ui created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import Header from './componets/Header/Header';
 import Hero from './componets/Hero/Hero';
 import Residencies from './componets/Residencies/Residencies';
+import Contact from './componets/Contact/Contact';
+
+
 function App() {
   return (
     <div>
       <Header/>
       <Hero/>
       <Residencies/>
+      <Contact/>
+     
     </div>
   );
 }
