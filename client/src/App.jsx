f8a6d0aa7f49b6e48c35361b0f7689983937badd
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-06 13:27:43 +0530

    footer ui created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './componets/Hero/Hero';
 import Residencies from './componets/Residencies/Residencies';
 import Contact from './componets/Contact/Contact';
 import GetStarted from './componets/GetStarted/GetStarted';
+import Footer from './componets/Footer/Footer';
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Residencies/>
       <Contact/>
       <GetStarted/>
+      <Footer/>
      
     </div>
   );
