17c34a83f7aea35f9f5824b4b6e9e6237cdf5fea
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-10 15:10:46 +0530

    remove bookings function

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,15 +1,12 @@
 import express from 'express';
-import { createUser } from '../controllers/user.Cntrl.js';
-import { bookVisit } from '../controllers/user.Cntrl.js';
-import  {getAllBookings} from '../controllers/user.Cntrl.js';
-
-
+import { createUser,  bookVisit, getAllBookings, cancelBooking } from '../controllers/user.Cntrl.js';
 
 const router = express.Router();  // ✅ Fixed typo
 
 router.post("/register", createUser);
 router.post("/bookVisit/:id",  bookVisit );
 router.post("/allBookings", getAllBookings);
+router.post("/removeBookings/:id",  cancelBooking);
 
 
 export { router as userRoute };  // ✅ Fixed export
