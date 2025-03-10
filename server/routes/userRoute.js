1896c16ae8cae1e1767d7575149f117de566b711
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-10 14:14:01 +0530

    residencey controlllers created

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -4,5 +4,6 @@ import { createUser } from '../controllers/user.Cntrl.js';
 const router = express.Router();  // ✅ Fixed typo
 
 router.post("/register", createUser);
+// router.post("/bookVisit/:id", bookVisit);
 
 export { router as userRoute };  // ✅ Fixed export
