8011c284b28ebc07a003ca1f8f019edb416adb9b
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-10 14:57:17 +0530

    book a visit to residency,all bookings controllers created

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { createUser } from '../controllers/user.Cntrl.js';
+import { bookVisit } from '../controllers/user.Cntrl.js';
+import  {getAllBookings} from '../controllers/user.Cntrl.js';
+
+
 
 const router = express.Router();  // ✅ Fixed typo
 
 router.post("/register", createUser);
-// router.post("/bookVisit/:id", bookVisit);
+router.post("/bookVisit/:id",  bookVisit );
+router.post("/allBookings", getAllBookings);
+
 
 export { router as userRoute };  // ✅ Fixed export
