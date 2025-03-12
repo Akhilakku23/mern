de5c694243b1bb72dfd582dd62740833f5d4b7f2
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-12 10:59:34 +0530

    fav residency controller created

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser,  bookVisit, getAllBookings, cancelBooking } from '../controllers/user.Cntrl.js';
+import { createUser,  bookVisit, getAllBookings, cancelBooking,  toFav } from '../controllers/user.Cntrl.js';
 
 const router = express.Router();  // ✅ Fixed typo
 
@@ -7,6 +7,7 @@ router.post("/register", createUser);
 router.post("/bookVisit/:id",  bookVisit );
 router.post("/allBookings", getAllBookings);
 router.post("/removeBookings/:id",  cancelBooking);
+router.post("/toFav/:rid", toFav);
 
 
 export { router as userRoute };  // ✅ Fixed export
