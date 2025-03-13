2ff2222ca68495bbeac9d10fb99cf49481297b65
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-13 13:19:36 +0530

    puffloader in copy and move it in residencies page

--- client/src/components/Residencies/Residencies.jsx
+++ client/src/components/Residencies/Residencies.jsx
@@ -5,9 +5,36 @@ import './Residencies.css'
 import data from '../../utils/slider.json'
 import { sliderSettings } from '../../utils/common'
 import PropertyCard from '../PropertyCard/PropertyCard'
+import useProperties from '../../hooks/useProperties'
+import { PuffLoader } from "react-spinners";
 
 
 const Residencies = () => {
+
+  const { data, isLoading, isError, refetch } = useProperties()
+
+  if (isError) {
+    return (
+      <div className="wrapper">
+        <span>Error while fetching data</span>
+      </div>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <div className="wrapper flexCenter" style={{ height: "60vh" }}>
+        <PuffLoader
+          height="80"
+          width="80"
+          radius={1}
+          color="#4066ff"
+          aria-label="puff-loading"
+        />
+      </div>
+    );
+  }
+
   return (
     <section className="r-wrapper">
       <div className="paddings innerWidth r-container">
@@ -20,7 +47,7 @@ const Residencies = () => {
         {/* slider */}
         <SliderButtons/>
 
-        {data.map((card,i) => (
+        {data.slice(0,8).map((card,i) => (
           <SwiperSlide key={i}>
           <PropertyCard card={card}/>
           </SwiperSlide>
