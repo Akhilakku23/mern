c9bbe8050311b18c97355166eebc463ef145adbf
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-14 11:49:40 +0530

    add Property page styles and implement property data fetching

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import { getProperty } from "../../utils/api";
+import { PuffLoader } from "react-spinners";
+import { AiFillHeart } from "react-icons/ai";
+import { useQuery } from "@tanstack/react-query";
+import "./Property.css";
 
 const Property = () => {
+  const { pathname } = useLocation();
+  const id = pathname.split("/").slice(-1)[0];
+  console.log(id);
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["resd", id],
+    queryFn: () => getProperty(id),
+    enabled: !!id,
+  });
+  console.log(data)
+
+  if (isLoading) {
+    return (
+      <div className="wrapper">
+        <div className="flexCenter paddings">
+          <PuffLoader />
+        </div>
+      </div>
+    );
+  }
+
+  if (isError) {
+    return (
+      <div className="wrapper">
+        <div className="flexCenter paddings">
+          <span>Error while fetching the property details</span>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="wrapper">
-        thi is an individual property path
-    </div>
-  )
-}
+      <div className="flexColStart paddings innerWidth property-container">
+        {/* Like button */}
+        <div className="like">
+          <AiFillHeart size={24} color="white" />
+        </div>
 
-export default Property
+        {/* Image */}
+        <img src={data?.image} alt="home image" />
+
+        <div className="flexCenter property-details">
+          {/* left */}
+          <div className="flexColStart left">
+            {/*  head */}
+
+            <div className="flexStart head">
+            <span className="primaryText">{data?.title}</span>
+              <span className="orangeText" style={{ fontSize: "1.5rem" }}>
+                $ {data?.price}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Property;
