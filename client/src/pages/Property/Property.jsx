802e1f89bee55fd3942e8c2390af1c42f41e92b2
Akhilakku23 <akhilsaji0031@gmail.com>
2025-03-19 12:34:20 +0530

    add useAuthCheck hook for authentication validation; integrate with Property page for booking functionality

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -4,7 +4,13 @@ import { getProperty } from "../../utils/api";
 import { PuffLoader } from "react-spinners";
 import { AiFillHeart } from "react-icons/ai";
 import { useQuery } from "@tanstack/react-query";
+import { FaShower } from "react-icons/fa";
+import { AiTwotoneCar } from "react-icons/ai";
+import { MdLocationPin, MdMeetingRoom } from "react-icons/md";
+import  Map  from "../../components/Map/Map"; 
 import "./Property.css";
+import useAuthCheck from "../../hooks/useAuthCheck";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Property = () => {
   const { pathname } = useLocation();
@@ -16,7 +22,10 @@ const Property = () => {
     queryFn: () => getProperty(id),
     enabled: !!id,
   });
-  console.log(data)
+ const [modalOpend, setModalOpend] = useState(false);
+ const{validteLogin} = useAuthCheck();
+ const {user} = useAuth0();
+ 
 
   if (isLoading) {
     return (
@@ -55,11 +64,70 @@ const Property = () => {
             {/*  head */}
 
             <div className="flexStart head">
-            <span className="primaryText">{data?.title}</span>
+              <span className="primaryText">{data?.title}</span>
               <span className="orangeText" style={{ fontSize: "1.5rem" }}>
                 $ {data?.price}
               </span>
             </div>
+
+            {/* facilities */}
+
+            <div className="flexStart facilities">
+              {/* bathrooms */}
+              <div className="flexStart facility">
+                <FaShower size={20} color="#1F3E72" />
+                <span>{data?.facilities?.bathrooms} Bathrooms</span>
+              </div>
+              {/* parkings */}
+              <div className="flexStart facility">
+                <AiTwotoneCar size={20} color="#1F3E72" />
+                <span>{data?.facilities.parkings} Parking</span>
+              </div>
+              {/* Bedrooms */}
+              <div className="flexStart facility">
+                <MdMeetingRoom size={20} color="#1F3E72" />
+                <span>{data?.facilities.bedrooms} Rooms</span>
+              </div>
+            </div>
+            {/* description */}
+
+            <span className="secondaryText" style={{ textAlign: "justify" }}>
+              {data?.description}
+            </span>
+
+            {/* address */}
+            <div className="flexStart">
+              <MdLocationPin size={25} />
+              <span className="seccondaryText">
+                {data?.address}
+                {data?.city}
+                {data?.country}
+              </span>
+            </div>
+            {/* Bookingbutton */}
+            <button className="button"
+            onClick={()=> {
+              validteLogin() && setModalOpend(true)
+            }}
+            >
+              Booking your visit
+            </button>
+
+            <BookingModal
+              opened={modalOpened}
+              setOpened={setModalOpened}
+              propertyId={id}
+              email={user?.email}
+            />
+
+          </div>
+  {/* right side */}
+  <div className="map">
+            <Map
+              address={data?.address}
+              city={data?.city}
+              country={data?.country}
+            />
           </div>
         </div>
       </div>
